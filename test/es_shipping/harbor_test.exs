b00a49855a2357453eac5a959a74e5f9c2a55f04
alexander-p30 <alexanderp30e@gmail.com>
2021-12-05 11:37:19 -0300

    test: harbor aggregate

---
+++ test/es_shipping/harbor_test.exs
@@ -0,0 +1,44 @@
+defmodule EsShipping.HarborTest do
+  use EsShipping.DataCase, async: true
+
+  import EsShipping.Factory
+
+  alias EsShipping.Harbor
+
+  @attrs %{name: "Sydney harbor", is_active: true, x_pos: 238, y_pos: 101_010}
+
+  setup_all do
+    %{command: build(:create_harbor, @attrs), event: build(:harbor_created, @attrs)}
+  end
+
+  describe "execute/2" do
+    test "return harbor created event when create harbor command is valid", ctx do
+      assert ctx.event == Harbor.execute(%Harbor{}, ctx.command)
+    end
+
+    test "return an error atom when create harbor command is invalid", ctx do
+      ctx = put_in(ctx.command.name, nil)
+      assert {:error, :must_have_name} == Harbor.execute(%Harbor{}, ctx.command)
+    end
+
+    test "raise when identity field is not nil and command is create harbor", ctx do
+      assert_raise FunctionClauseError, fn ->
+        Harbor.execute(%Harbor{name: "a name"}, ctx.command)
+      end
+    end
+  end
+
+  describe "apply/2" do
+    test "return a mutated harbor struct with event attributes", ctx do
+      mutated_harbor = %Harbor{
+        name: ctx.event.name,
+        is_active: ctx.event.is_active,
+        x_pos: ctx.event.x_pos,
+        y_pos: ctx.event.y_pos
+      }
+
+      assert mutated_harbor == Harbor.apply(%Harbor{}, ctx.event)
+      assert mutated_harbor == Harbor.apply(%Harbor{name: "some name"}, ctx.event)
+    end
+  end
+end
--- test/support/factory/harbors.ex
+++ test/support/factory/harbors.ex
@@ -1,8 +1,15 @@
 defmodule EsShipping.Factory.Harbors do
   defmacro __using__(_) do
     quote do
+      alias EsShipping.Harbor
       alias EsShipping.Harbors.Commands.CreateHarbor
       alias EsShipping.Harbors.Events.HarborCreated
+
+      def harbor_factory(attrs) do
+        harbor = %Harbor{name: "a name", is_active: true, x_pos: 0, y_pos: 0}
+
+        merge_attributes(harbor, attrs)
+      end
 
       def create_harbor_factory(attrs) do
         command = %CreateHarbor{name: "a name", is_active: true, x_pos: 0, y_pos: 0}
