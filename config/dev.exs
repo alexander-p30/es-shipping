ceee27932bade286120edd45bc2a21ae730c149b
alexander-p30 <alexanderp30e@gmail.com>
2021-12-18 18:18:27 -0300

    chore: setup event store in dev

--- config/dev.exs
+++ config/dev.exs
@@ -16,6 +16,16 @@ config :es_shipping, EsShippingWeb.Endpoint,
   secret_key_base: "BzDpJUtJzxtpR1SiHaou1wX8Ald/76g9Xxx57pdRWkn7icArjrtTZJOW8TjVxQBE",
   watchers: []
 
+config :es_shipping, EsShipping.EventSourcing.EventStore,
+  serializer: Commanded.Serialization.JsonSerializer,
+  username: "event_store_db",
+  password: "event_store_pass",
+  database: "event_store_db",
+  hostname: "localhost",
+  port: "2113",
+  show_sensitive_data_on_connection_error: true,
+  pool_size: 10
+
 config :logger, :console, format: "[$level] $message\n"
 
 config :phoenix, :stacktrace_depth, 20
