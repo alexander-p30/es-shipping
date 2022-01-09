b17e73ff852f61f81a6bf76709047d21160c9571
alexander-p30 <alexanderp30e@gmail.com>
2022-01-09 13:03:22 -0300

    chore: remove unecessary arity 1 factories

--- test/support/factory/harbors.ex
+++ test/support/factory/harbors.ex
@@ -1,59 +1,47 @@
 defmodule EsShipping.Factory.Harbors do
   alias EsShipping.Harbor
-  alias EsShipping.Harbor.Commands.Create
-  alias EsShipping.Harbor.Commands.Get
-  alias EsShipping.Harbor.Commands.Update
-  alias EsShipping.Harbor.Events.Created
-  alias EsShipping.Harbor.Events.Got
-  alias EsShipping.Harbor.Events.Updated
+  alias EsShipping.Harbor.Commands.{Create, Get, Update}
+  alias EsShipping.Harbor.Events.{Created, Got, Updated}
   alias EsShipping.Harbor.Projection
 
   defmacro __using__(_) do
     quote do
-      def harbor_factory(attrs) do
-        harbor = %Harbor{name: "a name", is_active: true, x_pos: 0, y_pos: 0}
-
-        merge_attributes(harbor, attrs)
+      def harbor_factory do
+        %Harbor{name: "a name", is_active: true, x_pos: 0, y_pos: 0}
       end
 
-      def harbor_projection_factory(attrs) do
-        projection = %Projection{
+      def harbor_projection_factory do
+        %Projection{
           id: Ecto.UUID.generate(),
           name: "a name",
           is_active: true,
           x_pos: 0,
           y_pos: 0
         }
-
-        merge_attributes(projection, attrs)
       end
 
-      def create_harbor_factory(attrs) do
-        command = %Create{
+      def create_harbor_factory do
+        %Create{
           id: Ecto.UUID.generate(),
           name: "a name",
           is_active: true,
           x_pos: 0,
           y_pos: 0
         }
-
-        merge_attributes(command, attrs)
       end
 
-      def harbor_created_factory(attrs) do
-        event = %Created{
+      def harbor_created_factory do
+        %Created{
           id: Ecto.UUID.generate(),
           name: "a name",
           is_active: true,
           x_pos: 0,
           y_pos: 0
         }
-
-        merge_attributes(event, attrs)
       end
 
-      def update_harbor_factory(attrs) do
-        command = %Update{
+      def update_harbor_factory do
+        %Update{
           id: Ecto.UUID.generate(),
           name: "some other name",
           is_active: true,
@@ -61,12 +49,10 @@ defmodule EsShipping.Factory.Harbors do
           y_pos: 987,
           received_fields: ~w(id name is_active x_pos y_pos)a
         }
-
-        merge_attributes(command, attrs)
       end
 
-      def harbor_updated_factory(attrs) do
-        event = %Updated{
+      def harbor_updated_factory do
+        %Updated{
           id: Ecto.UUID.generate(),
           name: "some other name",
           is_active: true,
@@ -74,14 +60,12 @@ defmodule EsShipping.Factory.Harbors do
           y_pos: 987,
           updated_fields: ~w(id name is_active x_pos y_pos)a
         }
-
-        merge_attributes(event, attrs)
       end
 
       def get_harbor_factory(attrs),
         do: merge_attributes(%Get{id: Ecto.UUID.generate()}, Map.take(attrs, [:id]))
 
-      def harbor_got_factory(attrs) do
+      def harbor_got_factory do
         event = %Got{
           id: Ecto.UUID.generate(),
           name: "some harbor",
@@ -89,8 +73,6 @@ defmodule EsShipping.Factory.Harbors do
           x_pos: 92_347,
           y_pos: 0
         }
-
-        merge_attributes(event, attrs)
       end
     end
   end
--- test/support/factory/ships.ex
+++ test/support/factory/ships.ex
@@ -1,20 +1,21 @@
 defmodule EsShipping.Factory.Ships do
+  alias EsShipping.Ship
+  alias EsShipping.Ship.Commands.{Create, Move}
+
   defmacro __using__(_) do
     quote do
-      def ship_factory(attrs) do
-        ship = %EsShipping.Ship{
+      def ship_factory do
+        %Ship{
           name: "HMS Something",
           is_active: true,
           is_docked: true,
           x_pos: 0,
           y_pos: 0
         }
-
-        merge_attributes(ship, attrs)
       end
 
-      def create_ship_factory(attrs) do
-        command = %EsShipping.Ship.Commands.Create{
+      def create_ship_factory do
+        %Create{
           id: Ecto.UUID.generate(),
           name: "Flying Dutchman",
           is_active: true,
@@ -22,8 +23,8 @@ defmodule EsShipping.Factory.Ships do
           x_pos: 0,
           y_pos: 0
         }
+      end
 
-        merge_attributes(command, attrs)
       def move_ship_factory do
         %Move{
           id: Ecto.UUID.generate(),
